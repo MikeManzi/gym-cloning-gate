b820ae06953a81b3b0d5862b43a1dd8568d6acca
MikeManzi <manzimike378@gmail.com>
2024-05-17 13:03:56 +0200

    add section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,29 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    container: {
-      center: false,
-      padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        md: "4rem",
-        lg: "6rem",
-        xl: "8rem",
-        "2xl": "12rem",
-        "3xl": "16rem",
-        "4xl": "20rem",
-      },
-      margin: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        md: "4rem",
-        lg: "6rem",
-        xl: "8rem",
-        "2xl": "12rem",
-        "3xl": "16rem",
-        "4xl": "20rem",
-      },
-    },
     extend: {
       colors: {
         primary: '#a18a4d',
